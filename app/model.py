f807b5afaa78b53af08fe7266a6f8fa630f4b946
vshevch <vlad.shev@gmail.com>
2018-11-15 19:50:14 -0500

    Create model.py

---
+++ app/model.py
@@ -0,0 +1,30 @@
+import psycopg2
+import queries
+
+# Not sure how to use getDB
+def getDB():
+    db = psycopg2.connect(
+        host="localhost",
+        database="mapproject",
+        user="vladshev",
+        password="root",
+    )
+    db.autocommit = True
+    return db.cursor()
+
+def addTerm(t):
+    db = getDB()
+    result = db.execute(queries.addTerm.format(t))
+    return result
+
+
+def removeTerm(t):
+    db = getDB()
+    result = db.execute(queries.removeTerm.format(t))
+    return result
+
+
+def allTerms():
+    db = getDB()
+    db.execute(queries.getAll)
+    return db.fetchall()
