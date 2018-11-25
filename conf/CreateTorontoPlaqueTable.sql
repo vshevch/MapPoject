6331af77eac6c5fc0a146ec98ca7125b990139bb
vshevch <vlad.shev@gmail.com>
2018-11-25 13:24:46 -0500

    Create CreateTorontoPlaqueTable.sql

---
+++ conf/CreateTorontoPlaqueTable.sql
@@ -0,0 +1,10 @@
+create table TorontoPlaques (
+ID SERIAL,
+Accession VARCHAR (20),
+Plaque_Subject VARCHAR (100),
+Year SMALLINT, 
+Plaque_Text VARCHAR,
+Ward SMALLINT,
+Address VARCHAR (100),
+Longitude SMALLINT,
+Latitude SMALLINT);
